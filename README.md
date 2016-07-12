8a11a7157cfffd8c8838cacdf25cc382b0a80b81
sobriodev <sobriodev@gmail.com>
2016-07-12 20:28:29 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# collection
+# Collection
 Collection library provides a simple way to access and modify nested arrays
---
+++ src/Collection.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maki
+ * Date: 12.07.16
+ * Time: 20:28
+ */
+
+namespace Collection;
+
+
+class Collection
+{
+
+}
