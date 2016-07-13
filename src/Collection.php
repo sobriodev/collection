d384536bbd95d8f60c6b21b4e8b6f5c18e089961
sobriodev <sobriodev@gmail.com>
2016-07-13 11:15:51 +0200

    Added get method

--- src/Collection.php
+++ src/Collection.php
@@ -11,5 +11,54 @@ namespace Collection;
 
 class Collection
 {
+    private $elements;
 
+    public function __construct(array $elements = [])
+    {
+        $this->elements = $elements;
+    }
+
+    public function getElements()
+    {
+        return $this->elements;
+    }
+
+    public function get(string $path)
+    {
+        $this->expectValidPath($path);
+
+        try {
+            return $this->search(explode('.', $path));
+        } catch (\OutOfBoundsException $e) {
+            echo $e->getMessage();
+            exit;
+        }
+    }
+
+    private function &search(array $keys, bool $byReference = false)
+    {
+        $response = $byReference ? $response = &$this->elements : $this->elements;
+
+        for ($i = 0, $lastIndex = count($keys) - 1; $i <= $lastIndex; $i++) {
+            $key = $keys[$i];
+            $keyExists = array_key_exists($key, $response);
+
+            if (!$keyExists || ($keyExists && $i !== $lastIndex && !is_array($response[$key]))) {
+                throw new \OutOfBoundsException(
+                    sprintf('%s key doesn\'t exist', $keyExists ? $keys[$i + 1] : $key)
+                );
+            }
+            $response = $byReference ? $response = &$response[$key] : $response[$key];
+        }
+        return $response;
+    }
+
+    private function expectValidPath(string $path)
+    {
+        if (!preg_match('/^(?:\p{L}|\d)+(?:\.(?:\p{L}|\d)+)*$/', $path)) {
+            throw new \InvalidArgumentException(
+                sprintf('%s isn\'t valid path', $path)
+            );
+        }
+    }
 }
